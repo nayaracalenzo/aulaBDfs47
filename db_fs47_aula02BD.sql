e352c01e1b7d6cb78c7c9542cf7367d61aba395b
nayaracalenzo <queiroz.14@hotmail.com>
2025-10-09 10:54:10 -0300

    aula funções e manipulação de dados SQL

--- db_fs47_aula02BD.sql
+++ db_fs47_aula02BD.sql
@@ -77,8 +77,59 @@ INSERT INTO produtos (nome, codigo, valor_unitario) VALUES
 
 SELECT * FROM produtos; 
 
+UPDATE produtos SET codigo = '010' WHERE id = 1;
+
+UPDATE produtos SET valor_unitario = 10.00;
+
+-- Dá erro pois viola a regra de valor único para email
+UPDATE clientes SET email = 'nayara@teste.com.br';
+
+DELETE FROM produtos WHERE nome = 'dipirona';
+
+-- consulta de uma coluna da tabela
+SELECT nome FROM clientes;
+
+-- Select com duas condições 
+SELECT * FROM clientes WHERE nome = 'Mota' AND email = 'mota2@teste.com' ;
+
+-- renomeando uma coluna (AS)
+SELECT COUNT(*) AS clientes FROM clientes;
+
+INSERT INTO produtos (nome, codigo, valor_unitario) VALUES 
+('balança', '004', 100.00),
+('cloridrato de ciclobenzaprina', '005', 15.00 ),
+('shampoo Dove', '100', 25.00);
+
+--SOMANDO TODOS DA COLUNA valor_unitario
+SELECT SUM(valor_unitario) FROM produtos;
+
+--SOMANDO COM FILTRO
+SELECT SUM(valor_unitario) FROM produtos WHERE nome IN ('shampoo Dove', 'balança');
+
+-- SELECT COM MÉDIA 
+SELECT AVG(valor_unitario) AS media_valor_unitario FROM produtos;
+
+-- SELECT MAX 
+SELECT nome, valor_unitario FROM produtos WHERE valor_unitario =
+(SELECT MAX(valor_unitario) FROM produtos);
 
 
+-- SELECT COM ORDENAÇÃO E LIMITE DE LINHAS
+SELECT nome, valor_unitario FROM produtos ORDER BY valor_unitario DESC LIMIT 1;
 
 
+ALTER TABLE produtos ADD COLUMN categoria VARCHAR(20);
 
+ALTER TABLE produtos DROP COLUMN categoria;
+
+SELECT * FROM produtos;
+
+-- ALTERAR O CAMPO CATEGORIA PASSANDO MEDICAMENTO, COSMETICO, UTILITARIO
+
+UPDATE produtos SET categoria = 'medicamento' WHERE id IN (2,3,5);
+
+UPDATE produtos SET categoria = 'cosmetico' WHERE id = 6;
+
+UPDATE produtos SET categoria = 'utilitario' WHERE id = 4;
+
+-- faça uma consulta contando quantos itens tem em cada categoria (group by)
